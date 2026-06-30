cb10f5cb813e914d0d941ac2c68a1762c5667c68
mohammedtaufeeqfb-ux <mohammedtaufeeqfb@gmail.com>
2026-06-30 22:29:02 +0530

    Add Terraform variable definitions

--- 01-terraform-ec2-provisioning/source-code/variables.tf
+++ 01-terraform-ec2-provisioning/source-code/variables.tf
@@ -1,1 +1,21 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
+}
 
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "EC2 Key Pair"
+  type        = string
+}

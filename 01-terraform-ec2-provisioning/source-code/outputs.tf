b3a85a59e30816b6013fa3fa76d5380a4a1a8f97
mohammedtaufeeqfb-ux <mohammedtaufeeqfb@gmail.com>
2026-06-30 22:31:42 +0530

    Add Terraform output values

--- 01-terraform-ec2-provisioning/source-code/outputs.tf
+++ 01-terraform-ec2-provisioning/source-code/outputs.tf
@@ -1,1 +1,9 @@
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.web.id
+}
 
+output "public_ip" {
+  description = "EC2 Public IP"
+  value       = aws_instance.web.public_ip
+}

8b4bf679309ff4aa913e9285be37dcd67cb97280
mohammedtaufeeqfb-ux <mohammedtaufeeqfb@gmail.com>
2026-06-30 22:24:18 +0530

    Update main.tf

--- 01-terraform-ec2-provisioning/source-code/main.tf
+++ 01-terraform-ec2-provisioning/source-code/main.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "terraform-demo-instance"
+  }
+}
